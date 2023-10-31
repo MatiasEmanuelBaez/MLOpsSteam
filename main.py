a24571cf003fb9b9af8e913bbe4b26719e4a87ad
Matias E. Baez <flaco_baez@hotmail.com>
2023-10-31 11:01:40 -0300

    prueba de funcion nro uno

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
-
 from fastapi import FastAPI
+
+import csv
 
 app = FastAPI()
 
@@ -9,6 +10,31 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/Resultados/{genero}")
+def PlayTimeGenre( genero : str ):
+    with open('Desarrollo/Resultados/PConsulta.csv', newline='') as File:
+        reader = csv.reader(File)
+        encontrar=0
+        for fila in reader:
+            if genero == fila[1]:
+                return ("Año de lanzamiento con más horas jugadas para Género",fila[1] ,":", fila[2])
+                encontrar=1
+                exit
+        
+        if encontrar == 0:
+            return("Género no encontrado")
+
+
+@app.get("/Resultados/{genero}")
+def UserForGenre( genero : str ):
+    with open('Desarrollo/Resultados/SConsulta.csv', newline='') as File:
+        reader = csv.reader(File)
+        encontrar=0
+        for fila in reader:
+            if genero == fila[1]:
+                return ("Año de lanzamiento con más horas jugadas para Género",fila[1] ,":", fila[2])
+                encontrar=1
+                exit
+        
+        if encontrar == 0:
+            return("Género no encontrado")
